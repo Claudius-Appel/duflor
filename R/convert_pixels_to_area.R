0833bce6985159bc80f90866ac030aa73e42c7f8
Claudius Appel <151634114+Claudius-Appel@users.noreply.github.com>
2024-04-04 17:35:07 +0200

    allow `convert_pixels_to_area()` to operate on a list of `pixel.counts`
    for
    - root_area
    - plant_area_complete
    - plant_area_green
    - plant_area_drought
    
    squash to 4210ddbeda7d7b6e0d5a7bf593723a7ed7555c34

--- R/convert_pixels_to_area.R
+++ R/convert_pixels_to_area.R
@@ -1,50 +1,80 @@
 
 #' convert pixel counts to an area based on the known area of an identifier-dot.
 #'
-#' @param pixel.counts list containing any of the following pixel-counts:
+#' @param pixel.counts A list containing any of the following pixel-counts:
 #'
 #' - "green"
 #' - "drought"
 #' - "complete"
 #' - "identifier"
 #'
+#' @description
+#' Convert pixel-counts to area based on declared area of the identifier
+#'
+#' @details
+#'
 #' The pixel-count for `identifier` must be present, whereas the others are optional.
 #' The calculated area is internally unit-less. However, as the default value for
-#' `duflor.default_identifier_area` is `r getOption("duflor.default_identifier_area")` \[cm^2\], the areas computed by this
+#' `duflor.declared_identifier_area` is `r getOption("duflor.default_identifier_area")` \[cm^2\], the areas computed by this
 #' function is also in \[cm^2\].
 #'
 #' The assumed area of the identifier can be modified by excuting
-#' `options(duflor.default_identifier_area = <value_in_square_centimeters>)` *prior* to calling this function
+#' `options(duflor.declared_identifier_area = <value_in_square_centimeters>)` *prior* to calling this function.
 #'
 #' @return list of computed areas for any key listed in `pixel.counts`.
 #'   Additionally, two meta-values are returned as well:
-#'   - the identifier-area used for calculating each area (whichever value is set in option `duflor.default_identifier_area`)
+#'   - the identifier-area used for calculating each area (whichever value is set in option `duflor.declared_identifier_area`)
 #'   - the area of a singe pixel
 #'
-#'   All values are in \[cm^2\]
+#' @note
+#' All values are in \[cm^2\].
 #'
 #' @export
 #' @importFrom utils hasName
 convert_pixels_to_area <- function(pixel.counts) {
-    default_identifier_area <- getOption("duflor.default_identifier_area")
+    if (is.null(getOption("duflor.declared_identifier_area"))) {
+        used_identifier_area <- getOption("duflor.default_identifier_area")
+    } else {
+        used_identifier_area <- getOption("duflor.declared_identifier_area")
+    }
     if (!hasName(pixel.counts,"identifier")) {
         stop(simpleError("FATAL: Unknown identifier count. Is the identifier missing?"))
     }
-    area_per_pixel = default_identifier_area / pixel.counts$identifier # see .onLoad()
-
+    if (hasName(pixel.counts$identifier,"pixel.count")) {
+        area_per_pixel = used_identifier_area / pixel.counts$identifier$pixel.count # see .onLoad()
+    } else {
+        area_per_pixel = used_identifier_area / pixel.counts$identifier # see .onLoad()
+    }
     ret <- list(area_per_pixel = area_per_pixel,
-                area_identifier = default_identifier_area)
+                area_identifier = used_identifier_area)
     if (hasName(pixel.counts,"green")) {
-        ret$plant_area_green <- area_per_pixel * pixel.counts$green
+        if (hasName(pixel.counts$green,"pixel.count")) {
+
+            ret$plant_area_green <- area_per_pixel * pixel.counts$green$pixel.count
+        } else {
+            ret$plant_area_green <- area_per_pixel * pixel.counts$green
+        }
     }
     if (hasName(pixel.counts,"drought")) {
-        ret$plant_area_drought <- area_per_pixel * pixel.counts$drought
+        if (hasName(pixel.counts$green,"pixel.count")) {
+            ret$plant_area_drought <- area_per_pixel * pixel.counts$drought$pixel.count
+        } else {
+            ret$plant_area_drought <- area_per_pixel * pixel.counts$drought
+        }
     }
     if (hasName(pixel.counts,"complete")) {
-        ret$plant_area_complete <- area_per_pixel * pixel.counts$complete
+        if (hasName(pixel.counts$green,"pixel.count")) {
+            ret$plant_area_complete <- area_per_pixel * pixel.counts$complete$pixel.count
+        } else {
+            ret$plant_area_complete <- area_per_pixel * pixel.counts$complete
+        }
     }
     if (hasName(pixel.counts,"root")) {
-        ret$root_area <- area_per_pixel * pixel.counts$root
+        if (hasName(pixel.counts$green,"pixel.count")) {
+            ret$root_area <- area_per_pixel * pixel.counts$root$pixel.count
+        } else {
+            ret$root_area <- area_per_pixel * pixel.counts$root
+        }
     }
     return(ret)
 }
