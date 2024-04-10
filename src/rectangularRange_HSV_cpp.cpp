6111f3ad07388636f95dd077b77bc99359cbaf37
Claudius Appel <151634114+Claudius-Appel@users.noreply.github.com>
2024-04-10 14:21:37 +0200

    `rectangularRange_HSV_cpp()`: added input validation, changed type of `upper_bound`/`lower_bound`

--- src/rectangularRange_HSV_cpp.cpp
+++ src/rectangularRange_HSV_cpp.cpp
@@ -19,6 +19,8 @@ using namespace Rcpp;
 //' The use of  [rectangularRange_HSV()] is strongly discouraged in favour of this function,
 //' due to its drastically slower execution.
 //'
+//' @seealso [rectangularRange_HSV_iteronce_cpp()]
+//'
 //' @inheritParams .main_args
 //' @param H respective component of a `pixel.array`
 //' @param S respective component of a `pixel.array`
@@ -29,19 +31,29 @@ using namespace Rcpp;
 //' - `pixel.idx` - pixel-locations of pixels detected between lower and upper bound.
 //'
 //' Upon failure to find any matching pixels, an empty matrix of dimensions `[0, 1:2]` is returned.
+//'
+//'
 //' @export
 // [[Rcpp::export]]
 DataFrame rectangularRange_HSV_cpp(NumericVector H,
                                    NumericVector S,
                                    NumericVector V,
-                                   std::vector<double> upper_bound,
-                                   std::vector<double> lower_bound,
+                                   NumericVector upper_bound,
+                                   NumericVector lower_bound,
                                    int image_width,
                                    bool check_V) {
+    // Rcout << "The value of v : " << l << "\n";
+    // Rcout << "The value of H : " << H.length() << "\n";
     int repetitions = H.size();
     std::vector<int> row_indices;
     std::vector<int> col_indices;
     if (check_V) {
+        if (upper_bound.length()!=3) {
+            stop("'upper_bound' must be a vector of 3 numerical values.");
+        }
+        if (lower_bound.length()!=3) {
+            stop("'lower_bound' must be a vector of 3 numerical values.");
+        }
         for (int i = 0; i < repetitions; i++) {
             if ((lower_bound[0] <= H[i]) && (H[i] <= upper_bound[0])
                     && (lower_bound[1] <= S[i]) && (S[i] <= upper_bound[1])
@@ -53,6 +65,12 @@ DataFrame rectangularRange_HSV_cpp(NumericVector H,
             }
         }
     } else {
+        if ((upper_bound.length()!=2) && (upper_bound.length()!=3)) {
+            stop("'upper_bound' must be a vector of 3 or 2 numerical values.");
+        }
+        if ((lower_bound.length()!=2) && (lower_bound.length()!=3)) {
+            stop("'lower_bound' must be a vector of 3 or 2 numerical values.");
+        }
         for (int i = 0; i < repetitions; i++) {
             if ((lower_bound[0] <= H[i]) && (H[i] <= upper_bound[0])
                     && (lower_bound[1] <= S[i]) && (S[i] <= upper_bound[1])) {
