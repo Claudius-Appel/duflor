6d853fd9d4e1e04b6a6d723c8eb86cd2f2c9f5a9
Claudius Appel <151634114+Claudius-Appel@users.noreply.github.com>
2024-04-11 08:01:24 +0200

    overhaul `rectangularRange_HSV_cpp()`: made inputs `upper_bound` and `lower_bound` nullable
    231303b83c87a2297045bd907b0374fc9cef7039

--- src/rectangularRange_HSV_cpp.cpp
+++ src/rectangularRange_HSV_cpp.cpp
@@ -38,8 +38,8 @@ using namespace Rcpp;
 DataFrame rectangularRange_HSV_cpp(NumericVector H,
                                    NumericVector S,
                                    NumericVector V,
-                                   NumericVector upper_bound,
-                                   NumericVector lower_bound,
+                                   Nullable<NumericVector> upper_bound,
+                                   Nullable<NumericVector> lower_bound,
                                    int image_width,
                                    bool check_V) {
     // Rcout << "The value of v : " << l << "\n";
@@ -47,35 +47,40 @@ DataFrame rectangularRange_HSV_cpp(NumericVector H,
     int repetitions = H.size();
     std::vector<int> row_indices;
     std::vector<int> col_indices;
+    // first check if the input bounds are NULL or not.
+    NumericVector upper_bound_;
+    NumericVector lower_bound_;
+    if (upper_bound.isNotNull()) {
+        upper_bound_ = upper_bound;
+    }
+    if (lower_bound.isNotNull()) {
+        lower_bound_ = lower_bound;
+    }
     if (check_V) {
-        if (upper_bound.length()!=3) {
+        if (upper_bound_.length()!=3) {
             stop("'upper_bound' must be a vector of 3 numerical values.");
         }
-        if (lower_bound.length()!=3) {
+        if (lower_bound_.length()!=3) {
             stop("'lower_bound' must be a vector of 3 numerical values.");
         }
         for (int i = 0; i < repetitions; i++) {
-            if ((lower_bound[0] <= H[i]) && (H[i] <= upper_bound[0])
-                    && (lower_bound[1] <= S[i]) && (S[i] <= upper_bound[1])
-                    && (lower_bound[2] <= V[i]) && (V[i] <= upper_bound[2])) {
-                int row = i % image_width;
-                int col = i / image_width;
+            if ((lower_bound_[0] <= H[i]) && (H[i] <= upper_bound_[0])
+                    && (lower_bound_[1] <= S[i]) && (S[i] <= upper_bound_[1])
+                    && (lower_bound_[2] <= V[i]) && (V[i] <= upper_bound_[2])) {
                 row_indices.push_back(row);
                 col_indices.push_back(col);
             }
         }
     } else {
-        if ((upper_bound.length()!=2) && (upper_bound.length()!=3)) {
+        if ((upper_bound_.length()!=2) && (upper_bound_.length()!=3)) {
             stop("'upper_bound' must be a vector of 3 or 2 numerical values.");
         }
-        if ((lower_bound.length()!=2) && (lower_bound.length()!=3)) {
+        if ((lower_bound_.length()!=2) && (lower_bound_.length()!=3)) {
             stop("'lower_bound' must be a vector of 3 or 2 numerical values.");
         }
         for (int i = 0; i < repetitions; i++) {
-            if ((lower_bound[0] <= H[i]) && (H[i] <= upper_bound[0])
-                    && (lower_bound[1] <= S[i]) && (S[i] <= upper_bound[1])) {
-                int row = i % image_width;
-                int col = i / image_width;
+            if ((lower_bound_[0] <= H[i]) && (H[i] <= upper_bound_[0])
+                    && (lower_bound_[1] <= S[i]) && (S[i] <= upper_bound_[1])) {
                 row_indices.push_back(row);
                 col_indices.push_back(col);
             }
