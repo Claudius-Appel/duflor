5d48d7f997618289b32306704c9316791df5f046
Claudius Appel <151634114+Claudius-Appel@users.noreply.github.com>
2024-04-11 08:03:36 +0200

    overhaul `rectangularRange_HSV_iteronce_cpp()`: made inputs `upper_bound` and `lower_bound` nullable `NumericMatrix`

--- src/rectangularRange_HSV_iteronce_cpp.cpp
+++ src/rectangularRange_HSV_iteronce_cpp.cpp
@@ -11,12 +11,15 @@ using namespace Rcpp;
 //' The use of  [rectangularRange_HSV()] & [rectangularRange_HSV_cpp()] is strongly discouraged in favour of this function,
 //' due to its drastically slower execution.
 //'
-//' @inheritParams .main_args
 //' @param H respective component of a `pixel.array`
 //' @param S respective component of a `pixel.array`
 //' @param V respective component of a `pixel.array`
 //' @param image_width Width of `pixel.array`, as returned via `dim(pixel.array)[1]`
 //' @param check_V boolean toggle to also check the `VALUE`-component of an HSV-pixel
+//' @param upper_bound EITHER:
+//' - **matrix** of upper HSV-bounds, e.g. `do.call(rbind,list(green = c(H_green_lower,S_green_lower,V_green_lower),drought = c(H_drought_lower,S_drought_lower,V_drought_lower)))`
+//' - single vector of length 3 declaring a set of HSV-values
+//' @param lower_bound see `upper_bound`
 //' @return A list-object with the following elements (when supplying one one pair of bounds)
 //' - `pixel.idx` - pixel-locations of pixels detected between lower and upper bound.
 //'
@@ -52,14 +55,48 @@ using namespace Rcpp;
 List rectangularRange_HSV_iteronce_cpp(NumericVector H,
                                        NumericVector S,
                                        NumericVector V,
-                                       NumericMatrix upper_bound,
-                                       NumericMatrix lower_bound,
+                                       Nullable<NumericMatrix> upper_bound,
+                                       Nullable<NumericMatrix> lower_bound,
                                        int image_width,
                                        bool check_V) {
     int repetitions = H.size();
     List results;
+    NumericMatrix upper_bound_;
+    NumericMatrix lower_bound_;
+    if (upper_bound.isNotNull()) {
+        upper_bound_ = static_cast<NumericMatrix>(upper_bound);
+    }
+    if (lower_bound.isNotNull()) {
+        lower_bound_ = static_cast<NumericMatrix>(lower_bound);
+    }
+    // Get the number of rows and columns if upper_bound is not null
+    int num_rows = 0;
+    // int num_cols = 0;
+    if (upper_bound.isNotNull()) {
+        num_rows = upper_bound_.rows();
+    }
 
-    for (int j = 0; j < upper_bound.rows(); j++) {
+    // Rcout << "upper.bound.cols(): " << upper_bound_.cols() << "\n";
+    if (check_V) {
+        // Rcout << "upper.bound.cols(): " << upper_bound_.cols() << "\n";
+        if (lower_bound_.cols()!=3) {
+            stop("'lower_bound' must be a matrix of N times 3 numerical values.");
+        }
+        if (upper_bound_.cols()!=3) {
+            stop("'upper_bound' must be a matrix of N times 3 numerical values.");
+        }
+    } else {
+        if ((upper_bound_.cols()!=2) && (upper_bound_.cols()!=3)) {
+            stop("'upper_bound' must be a matrix of N times 3 or 2 numerical values.");
+        }
+        if ((lower_bound_.cols()!=2) && (lower_bound_.cols()!=3)) {
+            stop("'lower_bound' must be a matrix of N times 3 or 2 numerical values.");
+        }
+    }
+    if (lower_bound_.cols()!=upper_bound_.cols()) {
+        stop("'upper_bound' and 'lower_bound' must have the same number of columns.");
+    }
+    for (int j = 0; j < num_rows; j++) {
         // re-init vectors for the indices
         std::vector<int> row_indices;
         std::vector<int> col_indices;
@@ -67,28 +104,13 @@ List rectangularRange_HSV_iteronce_cpp(NumericVector H,
         // Preallocate memory for indices vectors
         row_indices.reserve(repetitions);
         col_indices.reserve(repetitions);
-        if (check_V) {
-            if (upper_bound.cols()!=3) {
-                stop("'upper_bound' must be a vector of 3 numerical values.");
-            }
-            if (lower_bound.cols()!=3) {
-                stop("'lower_bound' must be a vector of 3 numerical values.");
-            }
-        } else {
-            if ((upper_bound.cols()!=2) && (upper_bound.cols()!=3)) {
-                stop("'upper_bound' must be a vector of 3 or 2 numerical values.");
-            }
-            if ((lower_bound.cols()!=2) && (lower_bound.cols()!=3)) {
-                stop("'lower_bound' must be a vector of 3 or 2 numerical values.");
-            }
-        }
         // store static bounds in variables
-        double lower_bound_H = lower_bound(j, 0);
-        double lower_bound_S = lower_bound(j, 1);
-        double lower_bound_V = lower_bound(j, 2);
-        double upper_bound_H = upper_bound(j, 0);
-        double upper_bound_S = upper_bound(j, 1);
-        double upper_bound_V = upper_bound(j, 2);
+        double lower_bound_H = lower_bound_(j, 0);
+        double lower_bound_S = lower_bound_(j, 1);
+        double lower_bound_V = lower_bound_(j, 2);
+        double upper_bound_H = upper_bound_(j, 0);
+        double upper_bound_S = upper_bound_(j, 1);
+        double upper_bound_V = upper_bound_(j, 2);
 
         for (int i = 0; i < repetitions; i++) {
             if (check_V) {
