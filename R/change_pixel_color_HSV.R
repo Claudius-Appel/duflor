961e1cc7e808dda627e7c5c1fdb59dabed87a941
Claudius Appel <151634114+Claudius-Appel@users.noreply.github.com>
2024-04-13 12:00:12 +0200

    merged `apply_hsv_color_to_image_subset()` into `change_pixel_color_HSV()`

--- R/change_pixel_color_HSV.R
+++ R/change_pixel_color_HSV.R
@@ -36,5 +36,17 @@ change_pixel_color_HSV <- function(pixel.array, pixel.idx, target.color, mask_ex
         target.color.hsv <- rgb2hsv(target.color[1], target.color[2], target.color[3])
     }
     ## colors are in HSV format here
-    return(apply_hsv_color_to_image_subset(pixel.array, pixel.idx, target.color.hsv, mask_extreme))
+    # it seems that converting the cImg to a normal arr speeds up the following operation
+    pa_t <- as.array(pixel.array)
+    for (i in 1:nrow(pixel.idx)) {
+        pa_t[pixel.idx[i, "x"], pixel.idx[i, "y"],1,1] <- target.color.hsv[1]
+        pa_t[pixel.idx[i, "x"], pixel.idx[i, "y"],1,2] <- target.color.hsv[2]
+    }
+    if (isTRUE(as.logical(mask_extreme))) {
+        for (i in 1:nrow(pixel.idx)) {
+            pa_t[pixel.idx[i, "x"], pixel.idx[i, "y"],1,3] <- target.color.hsv[3]
+        }
+    }
+    pixel.array <- as.cimg(pa_t)
+    return(pixel.array)
 }
